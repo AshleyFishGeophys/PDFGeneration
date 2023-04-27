27c713f82ce595258de313e617a3c8464bbf0381
Ashley Yaner <ashley.yaner@fractureid.com>
2023-04-27 17:16:41 -0600

    Day 24: Added lines to code

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ for idx, row in df.iterrows():
     # RGB. Below produces blue
     # pdf.set_text_color(0, 0, 254)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    # x1, y1, x2, y2
-    pdf.line(10, 21, 200, 21)
+    # define start and end of line x1, y1, x2, y2
+    # Adds lines on every page every 10mm
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     # Add break lines so that the footer is placed at the bottom of the page
@@ -37,5 +39,7 @@ for idx, row in df.iterrows():
         pdf.set_font(family="Times", style="B", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
